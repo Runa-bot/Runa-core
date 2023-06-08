b78995fac7656835659352c2911ea3a4b5fab11f
Kindit87 <deltaxteam78@gmail.com>
2023-06-08 14:48:26 +0500

    Gets a token from a file

---
+++ src/main/java/data/Token.java
@@ -0,0 +1,31 @@
+package data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Token {
+    public static final String TOKEN;
+
+    static {
+        try {
+            TOKEN = getTokenFromFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getTokenFromFile() throws IOException {
+        File file = new File("TOKEN.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            file.createNewFile();
+        }
+        String token = scanner.nextLine();
+        scanner.close();
+        return token;
+    }
+}
