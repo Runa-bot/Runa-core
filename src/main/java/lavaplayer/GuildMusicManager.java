743a6a0ffdb737cf3abdc6c6180d1cca6015a700
Kindit87 <deltaxteam78@gmail.com>
2023-06-09 07:05:09 +0500

    Made the sendHandler attribute public. Removed getSendHandler() method.

--- src/main/java/lavaplayer/GuildMusicManager.java
+++ src/main/java/lavaplayer/GuildMusicManager.java
@@ -7,16 +7,12 @@ public class GuildMusicManager {
 
     public final AudioPlayer audioPlayer;
     public final TrackScheduler scheduler;
-    private final AudioPlayerSendHandler sendHandler;
+    public final AudioPlayerSendHandler sendHandler;
 
     public GuildMusicManager(AudioPlayerManager manager) {
         this.audioPlayer = manager.createPlayer();
         this.scheduler = new TrackScheduler(this.audioPlayer);
         this.audioPlayer.addListener(this.scheduler);
         this.sendHandler = new AudioPlayerSendHandler(this.audioPlayer);
-    }
-
-    public AudioPlayerSendHandler getSendHandler() {
-        return this.sendHandler;
     }
 }
