963f3aa2d18d6d49cdbff1c3e0b8528f35b217fb
Kindit87 <deltaxteam78@gmail.com>
2023-06-11 20:37:42 +0500

    Parent command interface

--- src/main/java/com/kindit/bot/listeners/commands/Command.java
+++ src/main/java/com/kindit/bot/listeners/commands/Command.java
@@ -1,34 +1,10 @@
 package com.kindit.bot.listeners.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
-public abstract class Command extends ListenerAdapter {
-    private String name = "noNameCommand";
-    private String description = "noDescription";
+public interface Command {
+    CommandData getCommandData();
 
-    protected String getName() {
-        return name;
-    }
-
-    protected String getDescription() {
-        return description;
-    }
-
-    public Command(String name, String description) {
-        if (!name.equals("")) { this.name = name; }
-        if (!name.equals("")) { this.description = description; }
-    }
-
-    public CommandData getCommandData() {
-        return Commands.slash(name, description);
-    }
-
-    public CommandData getSubCommandsData() {
-        return Commands.slash(name, description);
-    }
-
-    public void interaction(SlashCommandInteractionEvent event) {}
+    void interaction(SlashCommandInteractionEvent event);
 }
