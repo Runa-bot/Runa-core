60560407f15dafbf112553b62c55a2d4e3510704
Kindit87 <deltaxteam78@gmail.com>
2023-06-11 20:49:55 +0500

    Interface to subcommands

---
+++ src/main/java/com/kindit/bot/listeners/commands/SubCommand.java
@@ -0,0 +1,11 @@
+package com.kindit.bot.listeners.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+public interface SubCommand {
+    String getName();
+    SubcommandData getSubCommandData();
+
+    void interaction(SlashCommandInteractionEvent event);
+}
