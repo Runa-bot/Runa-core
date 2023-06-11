53241ff553df4a2fed2a46e900c390502e7540e0
Kindit87 <deltaxteam78@gmail.com>
2023-06-11 20:40:16 +0500

    General module for music commands

---
+++ src/main/java/com/kindit/bot/listeners/commands/player/Player.java
@@ -0,0 +1,42 @@
+package com.kindit.bot.listeners.commands.player;
+
+import com.kindit.bot.listeners.commands.Command;
+import com.kindit.bot.listeners.commands.SubCommand;
+import com.kindit.bot.listeners.commands.player.subcommands.*;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements Command {
+    public final String name = "player";
+
+    private final SubCommand[] subCommands = {
+            new Add(),
+            new Skip(),
+            new Pause(),
+            new Resume(),
+            new Volume()
+    };
+    @Override
+    public CommandData getCommandData() {
+        List<SubcommandData> subcommandDataList = new ArrayList<>();
+
+        for (SubCommand command : subCommands) {
+            subcommandDataList.add(command.getSubCommandData());
+        }
+
+        return Commands.slash("player", "Audio player").addSubcommands(subcommandDataList);
+    }
+
+    @Override
+    public void interaction(SlashCommandInteractionEvent event) {
+        if (!event.getName().equals(name)) { return; }
+        for (SubCommand command : subCommands) {
+            if (event.getFullCommandName().equals(name + " " + command.getName())) { command.interaction(event); }
+        }
+    }
+}
