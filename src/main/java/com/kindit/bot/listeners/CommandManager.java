db44d4dbb7740654753387d0d1f27965f7f03094
Kindit87 <deltaxteam78@gmail.com>
2023-06-12 16:50:47 +0500

    Added ChatGPT() to the list of commands

--- src/main/java/com/kindit/bot/listeners/CommandManager.java
+++ src/main/java/com/kindit/bot/listeners/CommandManager.java
@@ -1,6 +1,7 @@
 package com.kindit.bot.listeners;
 
 import com.kindit.bot.listeners.commands.Command;
+import com.kindit.bot.listeners.commands.chat.ChatGPT;
 import com.kindit.bot.listeners.commands.player.Player;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -9,18 +10,26 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommandManager extends ListenerAdapter {
     private static final Command[] commands = {
-        new Player()
+        new Player(),
+        new ChatGPT()
     };
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         for (Command command : commands) {
-            command.interaction(event);
+            try {
+                command.interaction(event);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
