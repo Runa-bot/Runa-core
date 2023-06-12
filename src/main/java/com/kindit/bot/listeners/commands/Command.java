1823bba1875c3465264c5212eaa8a9dcb40cb894
Kindit87 <deltaxteam78@gmail.com>
2023-06-12 16:49:33 +0500

    Added getSubCommands method

--- src/main/java/com/kindit/bot/listeners/commands/Command.java
+++ src/main/java/com/kindit/bot/listeners/commands/Command.java
@@ -4,7 +4,9 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 public interface Command {
+    SubCommand[] getSubCommands();
+
     CommandData getCommandData();
 
-    void interaction(SlashCommandInteractionEvent event);
+    void interaction(SlashCommandInteractionEvent event) throws Exception;
 }
