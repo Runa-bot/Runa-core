47e98b4cac4feb37470b2b53138e38c3592afc9d
Kindit87 <deltaxteam78@gmail.com>
2023-11-25 17:40:11 +0500

    SubCommand for playlist

---
+++ src/main/java/com/kindit/bot/commands/playlist/subcommands/AddToUserPlaylistSubCommand.java
@@ -0,0 +1,46 @@
+package com.kindit.bot.commands.playlist.subcommands;
+
+import com.kindit.bot.commands.SubCommand;
+import com.kindit.bot.data.JsonUserPlaylistData;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+public class AddToUserPlaylistSubCommand extends SubCommand {
+    public AddToUserPlaylistSubCommand() {
+        super("add", "Add to yours playlist");
+    }
+
+    @Override
+    public SubcommandData getSubCommandData() {
+        return new SubcommandData(name, description)
+                .addOptions(
+                        new OptionData(OptionType.STRING, "url", "YouTube video URL", true),
+                        new OptionData(OptionType.STRING, "name", "Set name for url", true)
+                );
+    }
+
+    @Override
+    public void interaction(SlashCommandInteractionEvent event) throws Exception {
+        event.deferReply().setEphemeral(true).queue();
+
+        JsonUserPlaylistData playlistData = new JsonUserPlaylistData(event.getMember().getIdLong());
+        String url = event.getOption("url").getAsString();
+        String name = event.getOption("name").getAsString();
+        MessageEmbed embed = notSuccessfullyReplyEmbed();
+
+        if (!isYoutubeUrl(url)) embed = replyEmbed("Your link doesn't look like a link to a YouTube video", BAD_COLOR);
+        else if (isYoutubeUrl(name)) embed = replyEmbed("Your name looks like a link to a YouTube video", BAD_COLOR);
+        else playlistData.addUrl(name, url);
+
+        if (playlistData.isSet()) embed = successfullyReplyEmbed();
+
+        event.getHook().sendMessageEmbeds(embed).setEphemeral(true).queue();
+    }
+
+    private boolean isYoutubeUrl(String url) {
+        return url.contains("https://youtu.be/") || url.contains("https://www.youtube.com/");
+    }
+}
