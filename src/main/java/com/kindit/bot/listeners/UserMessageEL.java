977af570a8fc00dc861fd52dc8b082456798d02f
Kindit87 <deltaxteam78@gmail.com>
2023-11-27 23:08:31 +0500

    bug fix

--- src/main/java/com/kindit/bot/listeners/UserMessageEL.java
+++ src/main/java/com/kindit/bot/listeners/UserMessageEL.java
@@ -3,13 +3,15 @@ package com.kindit.bot.listeners;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Locale;
+
 public class UserMessageEL extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         long guildId = event.getGuild().getIdLong();
 
         if (guildId == 688479404862472263L || guildId == 907572465998381076L || guildId == 738134731560058922L) {
-            String message = event.getMessage().getContentRaw();
+            String message = event.getMessage().getContentRaw().toLowerCase(Locale.ROOT);
             String[] cummands = { "cum", "cam", "кам", "кум" };
 
             for (String word : cummands) {
