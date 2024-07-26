31f654cbea79c2a46bdf76a8ba965a3b2a67c03b
Kindit87 <deltaxteam78@gmail.com>
2024-07-26 09:49:39 +0300

    Listener to start initializing stuff

---
+++ src/main/java/xyz/kindit/nekobot/listener/LaunchListener.java
@@ -0,0 +1,14 @@
+package xyz.kindit.nekobot.listener;
+
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class LaunchListener extends ListenerAdapter {
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        super.onReady(event);
+    }
+
+}
