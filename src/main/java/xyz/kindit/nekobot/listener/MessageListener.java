a12065e5e2683e113250266ba474350c3b530b73
Kindit87 <deltaxteam78@gmail.com>
2024-07-26 09:43:18 +0300

    Init a listener to interact with messages

---
+++ src/main/java/xyz/kindit/nekobot/listener/MessageListener.java
@@ -0,0 +1,14 @@
+package xyz.kindit.nekobot.listener;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class MessageListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        super.onMessageReceived(event);
+    }
+
+}
