49dece94baff1fa19a12fefb277e909ab0125332
Kindit87 <deltaxteam78@gmail.com>
2024-09-09 15:55:50 +0300

    Cache user policy

--- src/main/java/xyz/kindit/runa/Runa.java
+++ src/main/java/xyz/kindit/runa/Runa.java
@@ -3,6 +3,8 @@ package xyz.kindit.runa;
 import lombok.Getter;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,13 @@ public class Runa {
 	@Getter
 	private static JDA jda;
 
-    public Runa(@Value("${runa.token}") String token) {
+    public Runa(@Value("${runa.token}") String token) throws InterruptedException {
 		jda = JDABuilder
 				.createDefault(token)
-				.build();
+				.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES)
+				.setMemberCachePolicy(MemberCachePolicy.ALL)
+				.build()
+				.awaitReady();
 	}
 
 	public static void main(String[] args) {
