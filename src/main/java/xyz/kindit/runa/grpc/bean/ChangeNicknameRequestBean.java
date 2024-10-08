ae8beced357040ebf7c9d0f9549b0dfa5f8fe8cc
Kindit87 <deltaxteam78@gmail.com>
2024-10-08 14:04:16 +0300

    JavaBean

---
+++ src/main/java/xyz/kindit/runa/grpc/bean/ChangeNicknameRequestBean.java
@@ -0,0 +1,23 @@
+package xyz.kindit.runa.grpc.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import xyz.kindit.runa.ChangeNicknameRequest;
+import xyz.kindit.runa.Runa;
+
+@Getter
+@AllArgsConstructor
+public class ChangeNicknameRequestBean {
+
+    public final Guild guild;
+    public final Member member;
+    public final String newNickname;
+
+    public <T> ChangeNicknameRequestBean(ChangeNicknameRequest request) {
+        guild = Runa.getJda().getGuildById(request.getGuildId());
+        member = guild.getMemberById(request.getMemberId());
+        newNickname = request.getNewNickname();
+    }
+}
