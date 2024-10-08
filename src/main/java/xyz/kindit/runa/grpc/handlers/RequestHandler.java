1ff4815f9ff1714794c87e39549fe5a1ff62ec66
Kindit87 <deltaxteam78@gmail.com>
2024-10-08 14:11:48 +0300

    Maybe it'll help.

---
+++ src/main/java/xyz/kindit/runa/grpc/handlers/RequestHandler.java
@@ -0,0 +1,16 @@
+package xyz.kindit.runa.grpc.handlers;
+
+import io.grpc.Status;
+
+public abstract class RequestHandler<B> {
+
+    public final B bean;
+    public final Status status = checkRequest();
+
+    protected RequestHandler(B bean) {
+        this.bean = bean;
+    }
+
+    protected Status checkRequest() { return null; };;
+
+}
